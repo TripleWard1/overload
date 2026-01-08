4d86a98ab34ea06473f3820984425dd044b948de
TripleWard1 <hugobarros93@gmail.com>
2026-01-08 14:10:20 +0000

    go

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,18 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Overload',
-  applicationName: 'Overload',
-  appleWebApp: {
-    title: 'Overload',
-    statusBarStyle: 'black-translucent',
-  },
-  themeColor: '#070B14',
+  title: "Overload",
+  applicationName: "Overload",
   icons: {
-    icon: 'https://i.imgur.com/jROIhp2.png',
-    apple: 'https://i.imgur.com/jROIhp2.png',
+    icon: "https://i.imgur.com/jROIhp2.png",
+    shortcut: "https://i.imgur.com/jROIhp2.png",
+    apple: "https://i.imgur.com/jROIhp2.png",
   },
+  themeColor: "#070B14",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt">
       <body>{children}</body>
